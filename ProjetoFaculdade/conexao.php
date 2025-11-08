46e142c65d3eeba03e46777e62c9ff7a87105a1b
edrobraga <136904639+edrobraga@users.noreply.github.com>
2025-11-08 19:07:52 -0300

    Add files via upload

---
+++ ProjetoFaculdade/conexao.php
@@ -0,0 +1,14 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$banco = "lootsy";
+$senha = "";
+
+$conexao = new mysqli($servidor,$usuario,$senha,$banco);
+
+if($conexao->connect_error){
+    echo("Erro de conexão");
+}
+echo "conexão efetuada";
+
+?>
