fd63a68f10d0fdaa2ca38182762490b55d042df8
Maria Rita Barros <mariaritabarros43@gmail.com>
2025-11-15 02:22:12 -0300

    Create usuario-create.php

---
+++ ProjetoFaculdade/usuario-create.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Usuários</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  </head>
+  <body>
+    <?php include('navbar.php'); ?>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Adicionar usuário
+                        <a href="index.php" class="btn btn-danger float-end">Voltar</a>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        <form action="acoes.php" method="POST">
+                            <div class="mb-3">
+                                <label>Nome</label>
+                                <input type="text" name="nome" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label>E-mail</label>
+                                <input type="text" name="email" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label>Data de Nascimento</label>
+                                <input type="date" name="data_nascimento" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label>Senha</label>
+                                <input type="password" name="senha" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                               <button type="submit" name="create_usuario" class="btn btn-primary">Salvar</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+</html>
