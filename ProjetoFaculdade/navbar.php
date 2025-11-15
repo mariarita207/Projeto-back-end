213fba1954849ee12d715e0d396d56c7f0aa1cd0
Maria Rita Barros <mariaritabarros43@gmail.com>
2025-11-15 02:21:42 -0300

    Add navbar with logo and sorting dropdown

---
+++ ProjetoFaculdade/navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar-dark" style="background-color: #392666;">
+    <div class="container-fluid d-flex align-items-center justify-content-between">
+        
+
+        <img 
+            src="https://raw.githubusercontent.com/mariarita207/Projeto-back-end/refs/heads/main/ProjetoFaculdade/assets/images/logo%2Bnome.png"
+            alt="Logo"
+            style="width: 150px; height: auto;"
+        >
+
+        <!-- DROPDOWN DE ORDENAÇÃO -->
+        <div class="dropdown">
+    <button class="btn btn-light btn-sm dropdown-toggle small" type="button" data-bs-toggle="dropdown">
+        Ordenar
+    </button>
+
+    <ul class="dropdown-menu dropdown-menu-end p-1 small">
+        <li><a class="dropdown-item small" href="index.php">Todos</a></li>
+        <li><a class="dropdown-item small" href="index.php?ordem=asc">Nome A–Z</a></li>
+    </ul>
+</div>
+
+    </div>
+</nav>
