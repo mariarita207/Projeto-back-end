17e053a84df157acb9bf2ce01324d236f1c50ab6
ElianSouto <e-fernandes@outlook.com>
2025-11-22 20:39:56 -0300

    Update index.js

--- ProjetoFaculdade/js/index.js
+++ ProjetoFaculdade/js/index.js
@@ -47,31 +47,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   mostrarUsuario();
 
-let trilho = document.getElementById('trilho')
-let body = document.querySelector('body')
 
-
-trilho.addEventListener('click', ()=>{
-trilho.classList.toggle('dark')
-body.classList.toggle('dark')
-
- // Salva o estado no localStorage
-if (body.classList.contains('dark')) {
-        localStorage.setItem('theme', 'dark');
-    } else {
-        localStorage.setItem('theme', 'light');
-    }
-
-})
-
-// Ao carregar a página, aplica o tema salvo
-window.addEventListener('DOMContentLoaded', () => {
-    const savedTheme = localStorage.getItem('theme');
-    
-    if (savedTheme === 'dark') {
-        document.body.classList.add('dark');
-        document.getElementById('trilho').classList.add('dark');
-    }
-});
 });
 // Fim do código
