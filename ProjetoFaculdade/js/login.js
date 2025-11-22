b7869f5c0c4f4835c87b8e8bfcf10fe4ac799446
ElianSouto <e-fernandes@outlook.com>
2025-11-22 20:13:35 -0300

    Update login.js tirando o darkmode do js

--- ProjetoFaculdade/js/login.js
+++ ProjetoFaculdade/js/login.js
@@ -60,29 +60,3 @@ function removeMensagem() {
   container.className = ""; // limpa estilos extras
 }
 
-let trilho = document.getElementById('trilho')
-let body = document.querySelector('body')
-
-
-trilho.addEventListener('click', ()=>{
-trilho.classList.toggle('dark')
-body.classList.toggle('dark')
-
- // Salva o estado no localStorage
-if (body.classList.contains('dark')) {
-        localStorage.setItem('theme', 'dark');
-    } else {
-        localStorage.setItem('theme', 'light');
-    }
-
-})
-
-// Ao carregar a página, aplica o tema salvo
-window.addEventListener('DOMContentLoaded', () => {
-    const savedTheme = localStorage.getItem('theme');
-    
-    if (savedTheme === 'dark') {
-        document.body.classList.add('dark');
-        document.getElementById('trilho').classList.add('dark');
-    }
-});
