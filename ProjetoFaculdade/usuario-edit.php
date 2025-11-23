872982e5e440de8ccfcd374ed2fdfb5e7c868474
Maria Rita Barros <mariaritabarros43@gmail.com>
2025-11-22 23:52:29 -0300

    Update 'Voltar' link to point to crud.php

--- ProjetoFaculdade/usuario-edit.php
+++ ProjetoFaculdade/usuario-edit.php
@@ -19,7 +19,7 @@ include 'conexao.php';
                 <div class="card">
                     <div class="card-header">
                         <h4>Editar usuário
-                        <a href="index.php" class="btn btn-danger float-end">Voltar</a>
+                        <a href="crud.php" class="btn btn-danger float-end">Voltar</a>
                         </h4>
                     </div>
                     <div class="card-body">
